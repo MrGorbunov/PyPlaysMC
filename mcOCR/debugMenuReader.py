bc2cdeba2a2d60c25bc725930325c4f8ae483ab3
MrGorbunov <volarcko@gmail.com>
2020-06-27 16:25:59 -0400

    OCR benchmarks calculated
    The debugMenuReader.py now has within it speeds
    of the different calls.

--- mcOCR/debugMenuReader.py
+++ mcOCR/debugMenuReader.py
@@ -9,14 +9,26 @@ import time
 input_img = cv2.imread("Debugmenu.png")
 input_img = input_img[:500, :800]
 
-text = read.read_image(input_img)
-print(text)
 
 print("Now trying to cache")
 y_to_save = read.get_line_y(input_img, "XYZ")
 print(y_to_save)
-print(read.read_at_line_y(input_img, y_to_save))
+# print(read.read_at_line_y(input_img, y_to_save))
 
+# The following nums are averages of 5 runs
+# 100 iterations of read_image = 6.667 sec
+# _1000_ iterations of read_at_line_y = 3.914 sec
+# 
+# Caching the line y and reading with it it is ~17x faster
+print("Now to get some metrics")
+iterations = 1000
+start_time = time.time()
+
+for i in range(iterations):
+    read.read_at_line_y(input_img, y_to_save)
+tot_time = start_time - time.time()
+
+print(tot_time)
 
 cv2.destroyAllWindows()
 
